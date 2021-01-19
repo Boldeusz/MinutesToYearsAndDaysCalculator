7604879b60f8c8d70be275f70922dff558a606e3
Boldeusz <mnpower@poczta.onet.pl>
2021-01-19 11:54:27 +0100

    update

--- src/MinutesToYearsAndDaysCalculator.java
+++ src/MinutesToYearsAndDaysCalculator.java
@@ -1,2 +1,11 @@
 public class MinutesToYearsAndDaysCalculator {
+    public static void printYearsAndDays(long minutes){
+        if (minutes < 0){
+            System.out.println("Invalid Value");
+        }
+        long days = minutes / 1440;
+        long remainingDays = days % 365;
+        long years = days / 365;
+        System.out.println(minutes + " min = " + years + " y and " + remainingDays + " d");
+    }
 }
